2dbfea6135551b1e2513bb5b1394fee54f191c9b
KuBeL <68178225+KuBeeL@users.noreply.github.com>
2021-01-19 15:07:30 +0100

    Create Xbee_emetteur.cpp

---
+++ Xbee_emetteur.cpp
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+SoftwareSerial xbee(2, 3);
+int count;
+
+void setup()
+{
+    xbee.begin(9600);                 
+    Serial.begin(9600);                     
+    count = 0;
+}
+
+void loop()
+{
+  String phrase = "Envoi du mot Sending";
+  phrase += count;
+  Serial.println(phrase);
+  xbee.println(phrase);
+  count++;
+  delay(300);
+}
