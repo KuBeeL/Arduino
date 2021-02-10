93f30bb048f7ce06b98f29a38a3b620f0811eda8
KuBeL <68178225+KuBeeL@users.noreply.github.com>
2021-02-10 11:01:59 +0100

    Update Xbee_Recepteur_trad.cpp
    Ajout de caractères

--- Xbee_Recepteur_trad.cpp
+++ Xbee_Recepteur_trad.cpp
@@ -2,8 +2,8 @@
 SoftwareSerial xbee(2, 3);
 
 void Trad(float n){
-  if(n == 13){
-    Serial.println('.');
+  if(n == 10){
+    Serial.println('');
   }
   if(n == 97){
     Serial.print('a');
@@ -116,7 +116,18 @@ void Trad(float n){
   if(n == 32){
     Serial.print(' ');
   }
-  
+  }
+  if(n == 124){
+    Serial.print('|');
+  }
+  }
+  if(n == 46){
+    Serial.print('.');
+  }
+  }
+  if(n == 38){
+    Serial.print('&');
+  }
 } 
 void setup()
 {
