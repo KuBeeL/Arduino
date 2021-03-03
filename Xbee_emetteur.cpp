07566e7ade8275f1418386c95c91b6f4d4f040af
KuBeL <68178225+KuBeeL@users.noreply.github.com>
2021-03-03 09:17:44 +0100

    Update Xbee_emetteur.cpp

--- Xbee_emetteur.cpp
+++ Xbee_emetteur.cpp
@@ -14,16 +14,21 @@ void setup()
 
 void loop()
 {
-  int valeurLue5 = analogRead(entreeAnalogique5);
-  int valeurLue10 = analogRead(entreeAnalogique10);
-  int valeurLue30 = analogRead(entreeAnalogique30);
+  analogWrite(A4, 500);
+  int valeurLue5 = analogRead(A1);
+  int valeurLue10 = analogRead(A2);
+  int valeurLue30 = analogRead(A3);
+  Serial.println(valeurLue5);
+  Serial.println(valeurLue10);
+  Serial.println(valeurLue30);
+  Serial.println("----------------");
   float tensionLue5 = map(valeurLue5, 0, 1023, 0, 500);
   float tensionLue10 = map(valeurLue10, 0, 1023, 0, 1000);
   float tensionLue30 = map(valeurLue30, 0, 1023, 0, 3000);
   
-  float tensionLue5 /= 100.0;
-  float tensionLue10 /= 100.0;
-  float tensionLue30 /= 100.0;  
+  tensionLue5 = tensionLue5/100.0;
+  tensionLue10 = tensionLue10/100.0;
+  tensionLue30 = tensionLue30/100.0;  
   String phrase = "Message";
   Serial.print("Borne 5v: ");
   Serial.print(tensionLue5);
@@ -34,7 +39,7 @@ void loop()
   Serial.print("Borne 30v: ");
   Serial.print(tensionLue30);
   Serial.println(" V");
-  xbee.println(tensionLue);
-  count++;
-  delay(300);
+  Serial.println("-----------");
+  //xbee.println(tensionLue);
+  delay(1000);
 }
